229603c2e1b5245628914447941fd48165845f67
Hectoralejandroperezlopez <hectoralejandro2503@gmail.com>
2025-04-14 14:11:24 -0500

    Add files via upload

---
+++ Practica 5 Do while/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace Practica_5_Do_while
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            listBox1 = new ListBox();
+            button1 = new Button();
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 25;
+            listBox1.Location = new Point(367, 105);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(180, 329);
+            listBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            button1.BackColor = SystemColors.ActiveBorder;
+            button1.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.Location = new Point(129, 181);
+            button1.Name = "button1";
+            button1.Size = new Size(140, 58);
+            button1.TabIndex = 1;
+            button1.Text = "Imprimir";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(25, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(489, 32);
+            label1.TabIndex = 2;
+            label1.Text = "Genera los numeros del 1 al 20 con While";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ActiveCaption;
+            ClientSize = new Size(549, 450);
+            Controls.Add(label1);
+            Controls.Add(button1);
+            Controls.Add(listBox1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ListBox listBox1;
+        private Button button1;
+        private Label label1;
+    }
+}
